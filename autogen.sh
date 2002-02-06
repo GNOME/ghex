e7647a41412ff7717cd7285683995d2708de70b9
Jaka Mocnik <jaka@src.gnome.org>
2002-02-06 07:48:55 +0000

    *** empty log message ***

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
+# Modified according to the new way of calling autogen.sh -- SnM 
+
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
@@ -13,4 +15,12 @@ PKG_NAME="GHex"
     exit 1
 }
 
-. $srcdir/macros/autogen.sh
+# Check for gnome-autogen.sh existance -- SnM 
+which gnome-autogen.sh || {
+   echo "You need to install gnome-common from the GNOME CVS"
+   exit 1
+}
+ 
+#. $srcdir/macros/autogen.sh -- SnM
+
+USE_GNOME2_MACROS=1 . gnome-autogen.sh
