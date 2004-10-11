6442620d1627848565a48133abdaea01004ab572
Jaka Mocnik <jaka@src.gnome.org>
2004-10-11 19:22:43 +0000

    *** empty log message ***

--- autogen.sh
+++ autogen.sh
@@ -9,7 +9,7 @@ test -z "$srcdir" && srcdir=.
 PKG_NAME="GHex"
 
 (test -f $srcdir/configure.in \
-  && test -f $srcdir/src/ghex.h) || {
+  && test -f $srcdir/src/ghex-window.h) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
     exit 1
