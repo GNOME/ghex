fb705c3901bd3d7c53fad6e62dbd08d2c4101311
Logan Rathbone <poprocks@gmail.com>
2021-12-20 09:53:42 -0500

    hex-buffer-*: correct copyright headers and add GPL

--- src/hex-buffer-iface.c
+++ src/hex-buffer-iface.c
@@ -1,12 +1,25 @@
 /* vim: ts=4 sw=4 colorcolumn=80                                                
  * -*- Mode: C; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- *
  */
-/* hex-buffer-iface.h - Generic buffer interface intended for use with the
+/* hex-buffer-iface.c - Generic buffer interface intended for use with the
  * HexDocument API
  *
  * Copyright © 2021 Logan Rathbone
  *
- * Original GHex author: Jaka Mocnik
+ * GHex is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * GHex is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GHex; see the file COPYING.
+ * If not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 #include "hex-buffer-iface.h"
--- src/hex-buffer-malloc.h
+++ src/hex-buffer-malloc.h
@@ -3,14 +3,25 @@
  */
 /* hex-document-malloc.h - `malloc` implementation of the HexBuffer iface.
  *
- * Based on code from aoeui, Copyright © 2007, 2008 Peter Klausler,
- * licensed by the author/copyright-holder under GPLv2 only.
- *
- * Source as adapted herein licensed for GHex to GPLv2+ with written
- * permission from Peter Klausler dated December 13, 2021 (see
- * associated git log).
- *
  * Copyright © 2021 Logan Rathbone
+ *
+ * Adapted from code originally contained in hex-document.c (see copyright
+ * notices therein for prior authorship).
+ *
+ * GHex is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * GHex is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GHex; see the file COPYING.
+ * If not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 #ifndef HEX_DOCUMENT_MALLOC_H
