GNOME/ghex

commit 5ba87a3e2ef8aa0e02e0784a659dae6648453727

HexWidget: Only alter payload length in insert mode
Delete, backspace, and cut operations now zero out bytes in overwrite
mode. In insert mode they can remove bytes from the document and shorten
the payload.

Additionally, delete and backspace now affect the entire selection,
while previously they would only affect one byte at a time.

Finally, set the cursor position with hex_widget_set_cursor() when
leaving insert mode. Previously hex_widget_set_insert_mode() would set
cursor_pos manually. As far as I know, this didn't cause any problems in
the last 22 years that it has been done this way[1], but it does cause
issues with the new code in this commit. I think it's more correct to
use hex_widget_set_cursor() to set the cursor position, otherwise the
selection info will be out of date and other side effects of that
function will be skipped.

[1]: d893211bfc7316719abc9b4f49e1e7fab1ac08a7

src/gtkhex.c
@@ -1814,8 +1814,11 @@ key_press_cb (GtkEventControllerKey *controller,
 	{
 		case GDK_KEY_BackSpace:
 			if (self->cursor_pos > 0) {
-				hex_document_set_data (self->document, self->cursor_pos - 1,
-						0, 1, NULL, TRUE);
+				if (self->insert)
+					hex_widget_delete_selection (self);
+				else
+					hex_widget_zero_selection (self);
+
 				if (self->selecting)
 					self->selecting = FALSE;
 				hex_widget_set_cursor (self, self->cursor_pos - 1);
@@ -1824,9 +1827,13 @@ key_press_cb (GtkEventControllerKey *controller,
 			break;
 
 		case GDK_KEY_Delete:
-			if (self->cursor_pos < payload_size) {
-				hex_document_set_data (self->document, self->cursor_pos,
-						0, 1, NULL, TRUE);
+			if (self->cursor_pos < payload_size)
+			{
+				if (self->insert)
+					hex_widget_delete_selection (self);
+				else
+					hex_widget_zero_selection (self);
+
 				hex_widget_set_cursor (self, self->cursor_pos);
 				ret = GDK_EVENT_STOP;
 			}
@@ -2391,7 +2398,10 @@ hex_widget_real_cut_to_clipboard(HexWidget *self,
 {
 	if (self->selection.start != -1 && self->selection.end != -1) {
 		hex_widget_real_copy_to_clipboard(self);
-		hex_widget_delete_selection(self);
+		if (self->insert)
+			hex_widget_delete_selection(self);
+		else
+			hex_widget_zero_selection(self);
 	}
 }
 
@@ -3270,6 +3280,31 @@ hex_widget_delete_selection (HexWidget *self)
 }
 
 /**
+ * hex_widget_zero_selection:
+ *
+ * Set the current selection to zero. The resulting action will be undoable.
+ *
+ * Since: 4.4
+ */
+void
+hex_widget_zero_selection (HexWidget *self)
+{
+	gint64 start, end;
+	size_t len;
+	char *zeros;
+
+	start = MIN(self->selection.start, self->selection.end);
+	end = MAX(self->selection.start, self->selection.end);
+
+	len = end - start + 1;
+	g_return_if_fail (len);
+
+	zeros = g_malloc0 (len);
+	hex_document_set_data (self->document, start, len, len, zeros, TRUE);
+	g_free (zeros);
+}
+
+/**
  * hex_widget_set_nibble:
  * @lower_nibble: %TRUE if the lower nibble of the current byte should be
  *   selected; %FALSE for the upper nibble
@@ -3564,7 +3599,7 @@ hex_widget_set_insert_mode (HexWidget *self, gboolean insert)
 	self->insert = insert;
 
 	if (!self->insert && self->cursor_pos > 0 && self->cursor_pos >= payload_size)
-			self->cursor_pos = payload_size - 1;
+			hex_widget_set_cursor (self, payload_size - 1);
 }
 
 /**

src/gtkhex.h
@@ -100,6 +100,7 @@ void hex_widget_set_selection (HexWidget *gh, gint64 start, gint64 end);
 gboolean hex_widget_get_selection (HexWidget *gh, gint64 *start, gint64 *end);
 void hex_widget_clear_selection (HexWidget *gh);
 void hex_widget_delete_selection (HexWidget *gh);
+void hex_widget_zero_selection (HexWidget *gh);
 
 HexWidgetAutoHighlight *
 hex_widget_insert_autohighlight (HexWidget *gh, const char *search, int len);

commit 0bf3188b029bfba6495ce8344e8671408169ca38

widget: Backspace should delete/zero the prior character if no selection
Fixes a minor regression from 5ba87a3e

src/gtkhex.c
@@ -2153,7 +2153,16 @@ key_press_cb (GtkEventControllerKey *controller,
 	switch(keyval)
 	{
 		case GDK_KEY_BackSpace:
-			if (self->cursor_pos > 0) {
+			if (self->cursor_pos > 0)
+			{
+				/* If we have only one character selected, what we want to do
+				 * is delete the prior character. Otherwise, this should
+				 * essentially work the same way as delete if we have a
+				 * highlighted selection.
+				 */
+				if (! hex_widget_get_selection (self, NULL, NULL))
+					hex_widget_set_cursor (self, self->cursor_pos - 1);
+
 				if (self->insert)
 					hex_widget_delete_selection (self);
 				else
@@ -2161,7 +2170,6 @@ key_press_cb (GtkEventControllerKey *controller,
 
 				if (self->selecting)
 					self->selecting = FALSE;
-				hex_widget_set_cursor (self, self->cursor_pos - 1);
 				ret = GDK_EVENT_STOP;
 			}
 			break;