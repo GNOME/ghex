7bbb1562b8a00fe1c06e8f6f924d42a7184b88d0
Logan Rathbone <poprocks@gmail.com>
2025-06-19 11:59:51 -0400

    hex-file-monitor: Add GPL header to source and header files
    Jordan Christiansen is the author of these files and adding the
    copyright headers was an oversight on MR !73.
    
    See discussion on GitLab on !73

--- src/hex-file-monitor.c
+++ src/hex-file-monitor.c
@@ -1,3 +1,27 @@
+/* vim: ts=4 sw=4 colorcolumn=80
+ */
+/* hex-file-monitor.c - Implementation of file monitor object
+ *
+ * Copyright © 2025 Jordan Christiansen
+ *
+ * GHex is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * GHex is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GHex; see the file COPYING.
+ * If not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Original GHex Author: Jaka Mocnik
+ */
+
 #include <stdio.h>
 
 #include "hex-file-monitor.h"
