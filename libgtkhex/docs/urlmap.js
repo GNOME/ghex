92b47f87f1c562f2eb6180ec524c12f273f5fd41
Logan Rathbone <poprocks@gmail.com>
2026-03-11 08:08:47 -0400

    docs: Get external links to work
    (cherry picked from commit 0457133909c5557a067e6f6c0c245cdb4be55a99)

---
+++ libgtkhex/docs/urlmap.js
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2021 GNOME Foundation
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+// Copied from libadwaita (which was: Copied from GTK)
+
+// A map between namespaces and base URLs for their online documentation
+baseURLs = [
+    [ 'GLib', 'https://docs.gtk.org/glib/' ],
+    [ 'Gio', 'https://docs.gtk.org/gio/' ],
+    [ 'GObject', 'https://docs.gtk.org/gobject/' ],
+    [ 'Gdk', 'https://docs.gtk.org/gdk4/' ],
+    [ 'Gsk', 'https://docs.gtk.org/gsk4/' ],
+    [ 'Gtk', 'https://docs.gtk.org/gtk4/' ],
+    [ 'Pango', 'https://docs.gtk.org/Pango/' ],
+]
