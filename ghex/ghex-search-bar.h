ea30b037916b4376613e388784765bc957aef748
Logan Rathbone <poprocks@gmail.com>
2026-06-09 14:49:03 -0400

    search: WIP - SQUASHME - good place to pause.

--- ghex/ghex-search-bar.h
+++ ghex/ghex-search-bar.h
@@ -10,7 +10,11 @@ G_DECLARE_FINAL_TYPE (GHexSearchBar, ghex_search_bar, GHEX, SEARCH_BAR, GHexPane
 
 GtkWidget *	ghex_search_bar_new (void);
 HexAutoHighlight * ghex_search_bar_get_auto_highlight (GHexSearchBar *self);
+void ghex_search_bar_set_selected_highlight (GHexSearchBar *self, guint selected_highlight);
+guint ghex_search_bar_get_selected_highlight (GHexSearchBar *self);
 void ghex_search_bar_set_replace_mode (GHexSearchBar *self, gboolean replace_mode);
 gboolean ghex_search_bar_get_replace_mode (GHexSearchBar *self);
+void ghex_search_bar_set_wraparound (GHexSearchBar *self, gboolean wraparound);
+gboolean ghex_search_bar_get_wraparound (GHexSearchBar *self);
 
 G_END_DECLS
--- libgtkhex/hex-highlight-list.h
+++ libgtkhex/hex-highlight-list.h
@@ -11,6 +11,7 @@ G_DECLARE_FINAL_TYPE (HexHighlightList, hex_highlight_list, HEX, HIGHLIGHT_LIST,
 
 HexHighlightList * hex_highlight_list_new (void);
 HexHighlight ** hex_highlight_list_get_highlights_for_range (HexHighlightList *self, gint64 start, gint64 end, guint *n_found);
+HexHighlight ** hex_highlight_list_get_highlights_for_offset (HexHighlightList *self, gint64 offset, guint *n_found);
 void hex_highlight_list_append (HexHighlightList *self, gpointer item);
 void hex_highlight_list_remove (HexHighlightList *self, HexHighlight *highlight);
 void hex_highlight_list_remove_all (HexHighlightList *self);
